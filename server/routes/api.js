368393b3d2158fbd96148e982790cb02e54e3ca1
spietari18 <spietari.18@gmail.com>
2021-12-07 20:12:35 +0200

    First versions of api endpoints

--- server/routes/api.js
+++ server/routes/api.js
@@ -10,23 +10,22 @@ const jwt = require("jsonwebtoken");
 const validateToken = require("../auth/validateToken");
 
 // Register
-router.get("/user/register", (req, res, next) => {
-    res.render("register");
-});
-
 router.post("/user/register",
-	    body("email").isEmail().normalizeEmail().escape(),
-	    body("password").isLength({min: 8})
-	    .matches('[a-z]').withMessage('Must contain a lowercase letter')
-	    .matches('[A-Z]').withMessage('Must contain a uppercase letter')
-	    .matches('[0-9]').withMessage('Must contain a number')
-	    .matches('[~`!@#$%^&*()-_+={}[]|\;:"<>,./?]')
+		// Do validation
+	    body("email").isEmail().normalizeEmail().escape(),					// Email
+	    body("password").isLength({min: 8})									// Password length
+	    .matches('[a-z]').withMessage('Must contain a lowercase letter')	// Password contains lowercase letter
+	    .matches('[A-Z]').withMessage('Must contain a uppercase letter')	// Password contains uppercase letter
+	    .matches('[0-9]').withMessage('Must contain a number')				// Password contains number
+	    .matches('[~`!@#$%^&*()-_+={}[]|\;:"<>,./?]')						// Password contains special character
 	    .withMessage('Must contain a special character'),
 	    (req, res, next) => {
+		// Password validation result, return possible errors
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 		    return res.status(400).json({errors: errors.array()});
 		}
+		// Check if email already in use
 		User.findOne({email: req.body.email}, (err, user) => {
 		    if (err) {
 			console.log(err);
@@ -35,6 +34,7 @@ router.post("/user/register",
 		    if (user) {
 			return res.status(403).json({email: "Email already in use."});
 		    } else {
+			// Generate password hash and store new user
 			bcrypt.genSalt(10, (err, salt) => {
 			    bcrypt.hash(req.body.password, salt, (err, hash) => {
 				if (err) throw err;
@@ -52,31 +52,33 @@ router.post("/user/register",
 	    });
 
 // Login
-router.get("/user/login", (req, res, next) => {
-    res.render("login");
-});
-
 router.post("/user/login",
+	// Normalize and escape email, same as in registeration
 	body("email").isEmail().normalizeEmail().escape(),
 	body("password"),
 	(req, res, next) => {
+		// Find user by email
 		User.findOne({email: req.body.email}, (err, user) => {
 		    if (err) throw err;
 		    if (!user) {
 			return res.status(403).json({message: "Login failed :("});
 		    } else {
+			// User found, compare password hashes
 			bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
 			    if (err) throw err;
 			    if (isMatch) {
+				// Create jsonwebtoken by using users id and email
 				const jwtPayload = {
 				    id: user._id,
 				    email: user.email
 				}
+				// Sign token with secret, set expiration
 				jwt.sign(jwtPayload,
 					 process.env.SECRET,
 					 {
 					     expiresIn: 240
 					 },
+					 // Return token on success
 					 (err, token) => {
 					     if (err) throw err;
 					     return res.json({success: true, token});
@@ -100,33 +102,95 @@ router.get("/post", (req, res, next) => {
 			return res.json({post});
 		}
 	})
-	// return all posts
 })
 
-// Post new post
+// Post a new post
 router.post("/post", validateToken, (req, res, next) => {
-	// return post id, success
-})
+	Post.create({
+		user: req.body.user,
+		post: req.body.post,
+		timestamp: Date.now()
+	}, (err, post) => {
+		if (err) throw err;
+		return res.status(200).json({post});
+	});
+});
 
-// Edit existing post
+// Edit an existing post
 router.post("/post/:id", validateToken, (req, res, next) => {
-	// return post id, success
+	Post.findOne({id: req.params.id}, (err, post) => {
+		if (err) {
+			if (err.name === "CastError") {
+				return res.status(404).send(`Post id ${req.params.id} not found!`);
+			}
+			return next(err);
+		}
+		if (post) {
+			Post.update({
+				_id: post.id
+			}, {
+				post: req.body.post
+			}, (err, post) => {
+				if (err) throw err;
+				if (post) {
+					return res.status(200).json({post});
+				}
+			})
+		} else {
+			return res.status(404).send(`Post id ${req.params.id} not found`);
+		}
+	})
 })
 
 // Comments
 // Get comment by id
 router.get("/comment/:id", (req, res, next) => {
-	// return comment
+	Comment.findOne({_id: req.params.id}, (err, comment) => {
+		if (err) {
+			if (err.name === "CastError") {
+				return res.status(404).send(`Post id ${req.params.id} not found`);
+			}
+			return next(err);
+		}
+		if (comment) {
+			return res.json({comment});
+		} else {
+			return res.status(404).send(`Post id ${req.params.id} not found`);
+		}
+	})
 })
 
 // Post new comment
 router.post("/comment", validateToken, (req, res, next) => {
-	// return comment id
+	Comment.create({
+		user: req.body.user,
+		comment: req.body.comment,
+		timestamp: Date.now()
+	}, (err, comment) => {
+		Post.findOne({_id: req.body.post_id}, (err, post) => {
+			if (err) throw err;
+			if (post) {
+				let comments = post.comments;
+				comments.push(comment._id)
+				Post.update({id: req.body.post_id}, {comments: comments},(err, post) => {
+					if (err) throw err;
+					if (post) {
+						return res.status(200).json(comment);
+					}
+				})
+			}
+		})
+	})
 })
 
 // Edit existing comment
 router.post("/comment/:id", validateToken, (req, res, next) => {
-	// return comment id, success
+	Comment.update({_id: req.params.id}, {comment: req.body.comment}, (err, comment) => {
+		if (err) throw err;
+		if (comment) {
+			return res.status(200).json(comment);
+		}
+	})
 })
 
 // Old endpoints, just for example!
